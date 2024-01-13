14e846681e9342c25eb5779710997f5160fd2f34
SundayNwovu <n.sunday@zarttech.com>
2024-01-13 17:58:31 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# HOW TO RUN THIS APP
+step 1. Get the Entire Project
+step 2. It's recommended that you setup and activate a virtual environment (Make sure you have python 3 installed ).
+You can activate virtual environment with the following steps:
+---> open the project in command prompt and type "python -m venv myven"
+---> myven\Scripts\activate.bat (on windows) or source myven/bin/activate (on Mac)
+step 3: run "pip install -r requirements.txt" to install all the dependencies for the project
+step 4: run "streamlit run code/main.py"
+
+A browser tab will open for you to start detecting fake urls with your desired models.
---
+++ code/main.py
@@ -0,0 +1,117 @@
+import pandas as pd 
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import tldextract
+from urllib.parse import urlparse
+from googlesearch import search
+import pickle
+import joblib
+
+import streamlit as st
+
+
+st.title('FAKE URL DETECTION')
+
+
+def letter_count(url):
+    letters = []
+    for i in url:
+        if i.isalpha():
+            letters.append(i)
+    return letters
+
+
+def digit_count(url):
+    digits = []
+    for i in url:
+        if i.isnumeric():
+            digits.append(i)
+    return digits
+
+def google_index(url):
+    site = search(url, 5)
+    return True if site else False
+
+
+def extract_features(url):
+    parsed_url = urlparse(url)
+    # Extract domain features using tldextract
+    domain_extract = tldextract.extract(url)
+    domain = domain_extract.domain
+    suffix = domain_extract.suffix
+    letters = letter_count(url)
+    digits = digit_count(url)
+    google_searchable = google_index(url)
+
+    # Features to extract
+    features = {
+        'url_length': len(url), #Length of URL
+        'domain_length': len(domain), #Length of domain
+        'dot_count': domain.count('.'), #Number of dots in the domain
+        'is_ip_address': domain.replace('.', '').isdigit(), #IP address in the domain
+        'special_chars_in_domain': any(char.isnumeric() or not char.isalnum() for char in domain), #Presence of special characters in the domain
+        'tld_length': len(suffix), #Length of the top-level domain (e.g., '.com', '.org')
+        'hyphen_in_domain': '-' in domain, #Presence of hyphen in the domain
+        'at_symbol': '@' in parsed_url.netloc, #Presence of '@' in the URL
+        'https': 'https' in parsed_url.scheme, #Check if site is secure with https
+        'letter_count': len(letters), #Length of alphabets in url
+        'digit_count': len(digits), #Length of numbers(digits) in url
+        'google_search': google_searchable
+    }
+
+    return features
+
+
+
+
+
+
+def model_predict(model, url):
+    class_mapping = {
+        0: 'Scam',
+        1: 'Not Scam'
+    
+    }
+    formated_url = extract_features(url)  
+    prediction = model.predict(np.array(list(formated_url.values())).reshape(1, -1))[0]
+    prediction = class_mapping.get(prediction, 'Unkown')
+    return prediction
+
+def main():
+
+    user_model = st.selectbox('Choose your model from the list', ['Select Model', 'Logistic Regression', 'Decision Tree', 'Random Forest'])
+
+    if user_model != 'Select Model':
+        # Load the model from the .h5 file
+        model_path = None
+
+        if user_model == 'Logistic Regression':
+            model_path = 'trained_model/Logistic_Regression.h5'
+        elif user_model == 'Decision Tree':
+            model_path = 'trained_model/Decision_Tree.h5'
+        elif user_model == 'Random Forest':
+            model_path = 'trained_model/Random_Forest.h5'
+
+        if model_path:
+            model = joblib.load(model_path)
+            input_url = st.text_input('Enter a website URL and our AI will tell you if it is scam or not')
+
+            if st.button('Check URL'):
+                if input_url:
+                    st.write('Hang tight while we check if the URL is scam or not scam')
+
+                    prediction = model_predict(model, input_url)
+
+                    st.write(f'Entered URL: {input_url}')
+                    st.write(f'Result: {prediction}')
+
+if __name__ == "__main__":
+    main()
